a765b377907d092d9878174c05f61e04aaecbafa
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-06-20 16:39:40 +0300

    designed structure for analytics summary card for each tracked category on the dashboard

--- web_dynamic/static/scripts/dashboard.js
+++ web_dynamic/static/scripts/dashboard.js
@@ -12,18 +12,29 @@ $(function () {
       $(this).remove();
     });
   $('div.percentage').each(function () {
-	  const percentage = parseFloat($(this).find('.percent').text());
-	  const circle = $(this).find('circle')[0];
+    const percentage = parseFloat($(this).find('.percent').text());
+    const circle = $(this).find('circle')[0];
 
-	  const radius = circle.r.baseVal.value;
-	  const circumference = 2 * Math.PI * radius;
-	  const offset = circumference - ((percentage / 100) * circumference);
-	  /* 0 offset means full circle */
-	  $(circle).css({
-		  'stroke-dasharray': circumference,
-		  'stroke-dashoffset': circumference
-	  });
+    const radius = circle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference - ((percentage / 100) * circumference);
+    /* 0 offset means full circle */
+    $(circle).css({
+      'stroke-dasharray': circumference,
+	    /** 'stroke-dashoffset': circumference**/
+	    'stroke-dashoffset': offset
 
-	    $(circle).animate({ 'stroke-dashoffset': offset }, 3000);
+    });
+
+	  /**    $(circle).animate({ 'stroke-dashoffset': offset }, 400); **/
+  });
+
+  $(this).find('.category-image').click(function () {
+	  console.log('image clicked');
+	 $(this).closest('.category-card').find('.analytics-card').css('display', 'block');
+  });
+  $(document).on('click', '#close-analytics', function () {
+    console.log('close clicked');
+    $(this).closest('.category-card').find('.analytics-card').css('display', 'none');
   });
 });
