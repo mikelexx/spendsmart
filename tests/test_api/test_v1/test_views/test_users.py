61d15aca95f368aa6e926fb75e28d1a4eeab70fa
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-08 21:05:04 +0300

    add testcase for get /users api

--- tests/test_api/test_v1/test_views/test_users.py
+++ tests/test_api/test_v1/test_views/test_users.py
@@ -23,6 +23,7 @@ def test_client():
 
 def test_post_user(test_client):
     """ Test POST /users api """
+    storage.reload()
     json = {
             "email": "user@example.com",
             "password": "strongpassword",
@@ -47,3 +48,30 @@ def test_post_user(test_client):
     #test creating duplicate user
     sec_user_response = test_client.post('/api/v1/users', json=json)
     assert sec_user_response.status_code == 409
+def test_get_users(test_client):
+    """ tests get api for /users"""
+    api_url = 'api/v1/users'
+    json = [{
+            "email": "user@example.com",
+            "password": "strongpassword",
+            "username": "user1"
+            },
+            {
+            "email": "user2@example.com",
+            "password": "strongpassword",
+            "username": "user2"
+            }
+            ]
+            
+    user1 = test_client.post(api_url, json=json[0])
+    users_response = test_client.get(api_url)
+    users_data = users_response.get_json()
+    assert len(users_data)  == len(storage.all(User))
+    for user in storage.all(User).values():
+        user.delete()
+        storage.save()
+    users_response = test_client.get(api_url)
+    users_data = users_response.get_json()
+    assert users_response.status_code == 200
+    assert len(users_data) == 0
+
